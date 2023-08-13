a2fa57a537a944ae2689d6ddba5dec5574b61fa5
Devopsnikhil4 <138021468+Devopsnikhil4@users.noreply.github.com>
2023-08-13 12:59:56 +0530

    Auto Push

--- README.md
+++ README.md
@@ -1,9 +1,5 @@
-# tf-module-redis
-
-```
+tf-module-redis
 Elastic Cache is a managed service offered in AWS for Redis.
 
 All the terraform code to provision Elastic Cache will be hosted in this repository.
 This will be the backend module, which will be called from the root module "terraform-databases"
-
-```
--- redis.tf
+++ redis.tf
@@ -1,29 +1,32 @@
-# Provisions Elastic Cache Cluster : Redis
-resource "aws_elasticache_cluster" "redis" {
-  cluster_id           = "roboshop-${var.ENV}-redis"
-  engine               = "redis"
-  node_type            = var.REDIS_INSTANCE_TYPE   
-  num_cache_nodes      = var.REDIS_INSTANCE_COUNT
-  parameter_group_name = aws_elasticache_parameter_group.redis_pg.name
-  engine_version       = var.REDIS_ENGINE_VERSION
-  port                 = var.REDIS_PORT_NUMBER
-  subnet_group_name    = aws_elasticache_subnet_group.redis_subnet_group.name
-  security_group_ids   = [aws_security_group.allows_redis.id]
-}
+# resource "aws_docdb_cluster" "docdb" {
+#   cluster_identifier       = "roboshop-${var.ENV}-docdb"
+#   engine                   = "docdb" 
+#   master_username          = "admin1"
+#   master_password          = "roboshop1"
+#   skip_final_snapshot      = true  
+#   vpc_security_group_ids   = [aws_security_group.allows_docdb.id]
+#   db_subnet_group_name     = aws_docdb_subnet_group.docdb_subnet_group.name
 
-# Creates Parameter Group needed for Elastic Cache
-resource "aws_elasticache_parameter_group" "redis_pg" {
-  name   = "roboshop-${var.ENV}-redis-pg"
-  family = "redis6.x"
-}
+# #   backup_retention_period = 14
+# #   preferred_backup_window = "02:00-05:00"
+# }
 
+# # creates subnet group 
+# resource "aws_docdb_subnet_group" "docdb_subnet_group" {
+#   name       = "roboshop-docdb-${var.ENV}-subnetgroup"
+#   subnet_ids = data.terraform_remote_state.vpc.outputs.PRIVATE_SUBNET_IDS
 
-# creates subnet group 
-resource "aws_elasticache_subnet_group" "redis_subnet_group" {
-  name       = "roboshop-redis-${var.ENV}-subnetgroup"
-  subnet_ids = data.terraform_remote_state.vpc.outputs.PRIVATE_SUBNET_IDS
+#   tags = {
+#     Name = "roboshop-docdb-${var.ENV}-subnetgroup"
+#   }
+# }
 
-  tags = {
-    Name = "roboshop-redis-${var.ENV}-subnetgroup"
-  }
-}
+# # Creates Compute Machines needed for DocumentDB and these has to be attached to the cluster
+# resource "aws_docdb_cluster_instance" "cluster_instances" {
+#   count              = 1
+#   identifier         = "roboshop-${var.ENV}-docdb-instance"
+#   cluster_identifier = aws_docdb_cluster.docdb.id                      #This argument attaches the nodes created here to the docdb cluster.
+#   instance_class     = "db.t3.medium"
+
+#   depends_on = [aws_docdb_cluster.docdb]
+# }
